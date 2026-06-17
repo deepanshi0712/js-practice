11c37500cc9139e259273fcb8688f169b450d53d
deepanshi0712 <pdeepanshi777@gmail.com>
2026-06-17 17:50:10 +0530

    exp6

---
+++ exp6.html/practice.html/practice01.js
@@ -0,0 +1,12 @@
+function generateColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+
+    document.body.style.backgroundColor = color;
+}
+
+document.getElementById("btn").addEventListener("click", generateColor);
